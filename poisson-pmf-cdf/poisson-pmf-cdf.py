f890e310c8176d0906e19ae83a759256800bde50
Manish Kumar <148943231+Manish-Kumar24@users.noreply.github.com>
2026-09-21 23:02:19 +0530

    Solve poisson-pmf-cdf

---
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -0,0 +1,9 @@
+import math
+
+def poisson_pmf_cdf(lam: float, k: int) -> dict:
+    term = math.exp(-lam)
+    cdf = term
+    for i in range(1, k+1):
+        term *= lam / i
+        cdf += term
+    return {"pmf":float(term), "cdf":float(cdf)}
