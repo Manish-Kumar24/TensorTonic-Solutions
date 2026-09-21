7b63602f91de5acba06961fab9c2cb148de93f8b
Manish Kumar <148943231+Manish-Kumar24@users.noreply.github.com>
2026-09-21 23:06:24 +0530

    Solve matrix-inverse

---
+++ matrix-inverse/matrix-inverse.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def matrix_inverse(A: list) -> np.ndarray | None:
+    matrix = np.asarray(A, dtype=float)
+    size = matrix.shape[0]
+    augmented = np.concatenate((matrix.copy(), np.eye(size)), axis=1)
+    for column in range(size):
+        pivot = column + np.argmax(np.abs(augmented[column:, column]))
+        if abs(augmented[pivot, column]) < 1e-12:
+            return None
+        augmented[[column, pivot]] = augmented[[pivot, column]]
+        augmented[column] /= augmented[column, column]
+        for row in range(size):
+            if row != column:
+                augmented[row] -= augmented[row, column] * augmented[column]
+    return augmented[:, size:]
