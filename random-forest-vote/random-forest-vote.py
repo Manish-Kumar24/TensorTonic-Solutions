40b8ff82b5fcc6e9ea22e80bf6c326c678076916
Manish Kumar <148943231+Manish-Kumar24@users.noreply.github.com>
2026-09-26 23:50:09 +0530

    Solve random-forest-vote

---
+++ random-forest-vote/random-forest-vote.py
@@ -0,0 +1,11 @@
+def random_forest_vote(predictions: list) -> list:
+    n_samples = len(predictions[0])
+    result = []
+    for i in range(n_samples):
+        votes = {}
+        for t in range(len(predictions)):
+            v = predictions[t][i]
+            votes[v] = votes.get(v, 0) + 1
+        mx = max(votes.values())
+        result.append(min(k for k, v in votes.items() if v == mx))
+    return result
